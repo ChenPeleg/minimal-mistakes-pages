690b17bb6eeb60595f143f110870c37d8fb79507
chenp <chenpeleg@gmail.com>
2023-02-07 10:47:22 +0200

    added error if no md file exists

--- docs/site/build-jekyll-site.sh
+++ docs/site/build-jekyll-site.sh
@@ -1,5 +1,14 @@
 #git clone git@github.com:whatever .
+echo "=== check for index.md file"
+if [ -e ./docs/index.md ]
+then
+    echo "found index.md file"
+else
+    echo "This process requires at leaset an docs/index.md file"
+    exit 1
+fi
 echo "=== copy docs to _tabs ==="
+mv ./docs/index.md ./index.md
 cp -r ./docs/. ./_tabs
 cd ./docs
 echo "=== added post dates to the docs files ==="
