f5d55dbae3c786e5a91d81845c3886a208fe8111
chenp <chenpeleg@gmail.com>
2023-02-07 10:27:05 +0200

    added a shell script

--- docs/site/build-jekyll-site.sh
+++ docs/site/build-jekyll-site.sh
@@ -1,7 +1,9 @@
 #git clone git@github.com:whatever .
-cp -r ./docs/site/. ./
+echo "=== copy docs to _tabs ==="
 cp -r ./docs/. ./_tabs
 cd ./docs
+echo "=== added post dates to the docs files ==="
 ls | xargs -I {} mv {} 2023-2-2-{}
 cd ..
+echo "=== copying the post files (with dates) to _posts ==="
 cp -r ./docs/. ./_posts
